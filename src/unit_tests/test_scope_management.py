1a28542ba666a0da7399001709546c1219238706
Matthew Frazer <frazerm@uci.edu>
2018-08-04 18:57:19 -0700

    add unit test declarations

---
+++ src/unit_tests/test_scope_management.py
@@ -0,0 +1,7 @@
+class TestScopeManagement:
+    # this also needs to be tested along a dependency heirarchy
+    def test_function_scope_dependency_obj_entry_is_deleted_after_delivered_to_function(self): pass
+    def test_instance_scope_dependency_obj_entry_is_deleted_after_delivered_to_instance(self): pass
+    def test_class_scope_allows_for_multiple_objects(self): pass
+    def test_module_scope_allows_for_multiple_objects(self): pass
+    def test_session_scope_does_not_allow_for_multiple_objects(self): pass
---
+++ src/unit_tests/test_service_location.py
@@ -0,0 +1,4 @@
+class TestServiceLocation:
+    def test_same_node_has_differing_scopes(self): pass
+    def test_same_node_is_utilized_by_two_different_nodes(self): pass
+    def test_container_scope_override(self): pass
